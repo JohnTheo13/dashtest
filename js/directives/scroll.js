81352f35900655846b6cea8df1a133904e972cde
Theodosiou Ioannis <theodosioujohn88@gmail.com>
2016-11-02 12:27:10 +0100

    Update scroll.js

--- js/directives/scroll.js
+++ js/directives/scroll.js
@@ -2,13 +2,3 @@ app.directive("scrollEnd",["$window", function($window){
 
  }]);
 
-/*
-    angular.element($window).bind("scroll", function() {
-    var windowHeight = "innerHeight" in window ? window.innerHeight : document.documentElement.offsetHeight;
-    var body = document.body, html = document.documentElement;
-    var docHeight = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight,  html.scrollHeight, html.offsetHeight);
-    windowBottom = windowHeight + window.pageYOffset;
-    if (windowBottom >= docHeight) {
-        alert('bottom reached'); }
-     });
-*/
